95b11dffa81b2f3e2c1b95cad00afaa98a7aa249
Pankaj Singh <psrathore13101@gmail.com>
2025-05-22 20:28:54 +0530

    enroll courses

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
-const {registerUser, loginUser,logoutUser,userProfile} = require('../cotrollers/user.controller');
+const {registerUser, loginUser, logoutUser, userProfile, enrollCourse, getEnrolledCourses} = require('../cotrollers/user.controller');
 const { authMiddleware } = require('../middleware/auth.middleware');
 
 router.post('/register', registerUser);
-router.post('/login',loginUser);
-router.post('/logout',authMiddleware,logoutUser)
-router.get('/profile',authMiddleware,userProfile);
+router.post('/login', loginUser);
+router.post('/logout', authMiddleware,logoutUser)
+router.get('/profile', authMiddleware,userProfile);
+router.post('/enroll/:courseId', authMiddleware, enrollCourse);
+router.get('/courses', authMiddleware, getEnrolledCourses);
 
 module.exports = router;
